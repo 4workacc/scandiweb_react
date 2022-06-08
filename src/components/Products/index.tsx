1fd230669e07c80388f7e6bc7e6d686e3c7068bc
Eugen <58693140+4workacc@users.noreply.github.com>
2022-06-08 10:39:26 +0300

    Update index.tsx
    fix displaying by cathegory

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -48,23 +48,23 @@ class Products extends Component<TProps, TState> {
        this.generateNewDisplayArr(this.state.products!, 1);
     }
     generateNewDisplayArr( products: IProduct[], step: number){
-        let newArr:any[] = [];
-        products.map ( (product: IProduct) => {
-            if ( product.category === this.props.curCathegory ) {
-            newArr.push(< Product 
-                            title={product.title}
-                            subtitle = {product.subtitle}
-                            price = {product.price}
-                            info = {product.info}                            
-                            key = {product.id}
-                            id = { product.id! }
-                            />)
-            }
-        });        
-        this.setState({
-            ...this.state,            
-            displayedArr: newArr,            
-        });
+        // let newArr:any[] = [];
+        // products.map ( (product: IProduct) => {
+        //     if ( product.category === this.props.curCathegory ) {
+        //     newArr.push(< Product 
+        //                     title={product.title}
+        //                     subtitle = {product.subtitle}
+        //                     price = {product.price}
+        //                     info = {product.info}                            
+        //                     key = {product.id}
+        //                     id = { product.id! }
+        //                     />)
+        //     }
+        // });        
+        // this.setState({
+        //     ...this.state,            
+        //     displayedArr: newArr,            
+        // });
         if (step === 0) {this.setState({products: products})};
     }
 
@@ -74,7 +74,18 @@ class Products extends Component<TProps, TState> {
                 { this.state.isShowCartMini && <CartMini />}
                 <h1 className="Products_CathegoryName">{this.props.curCathegory}</h1>               
                 <div className="Products_Cards">
-                    { this.state.displayedArr  }
+                    { this.state.products?.map( (product: IProduct) => {
+                        if ( product.category === this.props.curCathegory) {
+                            return (< Product 
+                                        title={product.title}
+                                        subtitle = {product.subtitle}
+                                        price = {product.price}
+                                        info = {product.info}                            
+                                        key = {product.id}
+                                        id = { product.id! }
+                                />)
+                        }
+                    }) }
                 </div>
             </div>
         )
