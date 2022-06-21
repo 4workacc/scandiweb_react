cc3da3006fbe7c1085c848969fcabcca00f74965
4workacc <58693140+4workacc@users.noreply.github.com>
2022-06-21 09:18:35 +0300

    Update index.tsx

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -19,6 +19,8 @@ type TState = {
     displayedArr: any[]   
 }
 
+// test graphql server  https://github.com/4workacc/scandiweb_node
+
 const client = new ApolloClient({
     uri: "http://localhost:4000/",
     cache: new InMemoryCache()
